fde5f86f9c462ada723608e80c1e94d75a0e8cc5
Maurizio Piroddi <x.hacker.hiphop99@gmail.com>
2026-02-24 22:55:28 +0100

    Miglioramenti alla grafica del sito

--- src/components/MovieCards.jsx
+++ src/components/MovieCards.jsx
@@ -6,13 +6,13 @@ function MovieCards(props) {
 
     return (
         <>
-            <div className="w-27">
-                <div className="w-100 h-70">
-                    <img className="w-50 h-50" src={imageString} alt="" />
+            <div className="w-27 bg-secondary p-3 my-4">
+                <div className="w-100 h-50 d-flex align-items-center justify-content-center">
+                    <img className="w-80 h-80" src={imageString} alt="" />
                 </div>
                 <div className="h-100 h-30">
-                    <h2>{title}</h2>
-                    <p>{description}</p>
+                    <h2 className="fs-4">{title}</h2>
+                    <p className="fs-6">{description}</p>
                 </div>
             </div>
         </>
